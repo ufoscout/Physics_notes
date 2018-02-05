c7ce8dc538e36b56888f356c6187f00cac14ef2f
ufoscout <ufoscout@gmail.com>
2018-02-05 12:32:06 +0100

    Update clean.sh script

--- clean.sh
+++ clean.sh
@@ -1,4 +1,6 @@
 find . -name '*.aux' -delete
 find . -name '*.log' -delete
+find . -name '*.out' -delete
 find . -name '*.pdf' -delete
 find . -name '*.synctex.gz' -delete
+find . -name '*.toc' -delete
